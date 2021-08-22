9906edb2d5eaa55cad5193e302a846e69802cee8
Ben Hession <bshession@gmail.com>
2021-08-22 17:36:42 +0100

    Complete steps 1-3

--- src/main/java/com/answerdigital/colourstest/controller/PeopleController.java
+++ src/main/java/com/answerdigital/colourstest/controller/PeopleController.java
@@ -1,15 +1,15 @@
 package com.answerdigital.colourstest.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import com.answerdigital.colourstest.dto.PersonUpdateDTO;
 import com.answerdigital.colourstest.exception.NotImplementedException;
@@ -23,32 +23,41 @@ public class PeopleController {
     @Autowired
     private PeopleRepository peopleRespository;
 
+    private Logger logger = LoggerFactory.getLogger("People Controller");
+
     @GetMapping
     public ResponseEntity<List<Person>> getPeople() {
-        // TODO STEP 1
+        // STEP 1
         //
         // Implement a JSON endpoint that returns the full list
         // of people from the PeopleRepository. If there are zero
         // people returned from PeopleRepository then an empty
         // JSON array should be returned.
-        throw new NotImplementedException();
+
+        return new ResponseEntity<>(peopleRespository.findAll(), HttpStatus.OK);
+
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Person> getPerson(@PathVariable("id") long id) {
-        // TODO: Step 2
+        // Step 2
         //
         // Implement a JSON endpoint that returns a single person
         // from the PeopleRepository based on the id parameter.
         // If null is returned from the PeopleRepository with
         // the supplied id then a NotFound should be returned.
 
-        throw new NotImplementedException();
+        Optional<Person> thePerson = peopleRespository.findById(id);
+
+        return thePerson
+                .map(person -> new ResponseEntity<>(person, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
-    @PutMapping("/{id}")
+    @Transactional
+    @PatchMapping(path = "/{id}", consumes = "application/json")
     public ResponseEntity<Person> updatePerson(@PathVariable("id") Long id, @RequestBody PersonUpdateDTO personUpdate) {
-        // TODO STEP 3
+        // STEP 3
         //
         // Implement an endpoint that recieves a JSON object to
         // update a person using the PeopleRepository based on
@@ -56,7 +65,30 @@ public class PeopleController {
         // updated, the person should be returned from the endpoint.
         // If null is returned from the PeopleRepository then a
         // NotFound should be returned.
-        throw new NotImplementedException();
+
+        Optional<Person> thePerson = peopleRespository.findById(id);
+
+        if (thePerson.isPresent()) {
+            Person person = thePerson.get();
+
+            person.setAuthorised(personUpdate.isAuthorised());
+            person.setEnabled(person.isEnabled());
+            person.setColours(personUpdate.getColours());
+
+            try {
+                Person savedPerson = peopleRespository.save(person);
+
+                return new ResponseEntity<>(savedPerson, HttpStatus.OK);
+
+            } catch (Exception e) {
+                logger.warn("Error saving person: ".concat(e.getMessage()));
+
+                return new ResponseEntity<>(null, HttpStatus.PRECONDITION_FAILED);
+            }
+
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
 }
