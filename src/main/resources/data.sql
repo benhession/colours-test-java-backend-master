b8e9429e777c029a22b41656262617f2a854e880
Ben Hession <bshession@gmail.com>
2021-08-22 20:38:53 +0100

    Remove inserts so generated ids can function as expected.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,50 +22,50 @@ CREATE TABLE person_colours (
 	FOREIGN KEY (colour_id) REFERENCES colour(id)
 );
 
-INSERT INTO colour 
- (id, name) 
-VALUES 
- (1, 'Red'),
- (2, 'Green'),
- (3, 'Blue');
- 
-INSERT INTO people 
- (id, first_name, last_name, authorised, enabled) 
-VALUES 
- (1, 'Bo', 'Bob', true, false),
- (2, 'Brian', 'Allen', true, true),
- (3, 'Courtney', 'Arnold', true, true),
- (4, 'Gabriel', 'Francis', false, false),
- (5, 'George', 'Edwards', true, false),
- (6, 'Imogen', 'Kent', false, false),
- (7, 'Joel', 'Daly', true, true),
- (8, 'Lilly', 'Hale', false, false),
- (9, 'Patrick', 'Kerr', true, true),
- (10, 'Sharon', 'Halt', false, false),
- (11, 'Willis', 'Tibbs', true, false);
- 
-INSERT INTO person_colours 
- (person_id, colour_id) 
-VALUES 
- (1, 1),
- (2, 1),
- (2, 2),
- (2, 3),
- (3, 1),
- (4, 2),
- (5, 2),
- (5, 3),
- (6, 1),
- (6, 2),
- (7, 2),
- (8, 1),
- (8, 2),
- (8, 3),
- (9, 2),
- (10, 1),
- (10, 2),
- (10, 3),
- (11, 1),
- (11, 2),
- (11, 3);
+-- INSERT INTO colour
+--  (id, name)
+-- VALUES
+--  (1, 'Red'),
+--  (2, 'Green'),
+--  (3, 'Blue');
+--
+-- INSERT INTO people
+--  (id, first_name, last_name, authorised, enabled)
+-- VALUES
+--  (1, 'Bo', 'Bob', true, false),
+--  (2, 'Brian', 'Allen', true, true),
+--  (3, 'Courtney', 'Arnold', true, true),
+--  (4, 'Gabriel', 'Francis', false, false),
+--  (5, 'George', 'Edwards', true, false),
+--  (6, 'Imogen', 'Kent', false, false),
+--  (7, 'Joel', 'Daly', true, true),
+--  (8, 'Lilly', 'Hale', false, false),
+--  (9, 'Patrick', 'Kerr', true, true),
+--  (10, 'Sharon', 'Halt', false, false),
+--  (11, 'Willis', 'Tibbs', true, false);
+--
+-- INSERT INTO person_colours
+--  (person_id, colour_id)
+-- VALUES
+--  (1, 1),
+--  (2, 1),
+--  (2, 2),
+--  (2, 3),
+--  (3, 1),
+--  (4, 2),
+--  (5, 2),
+--  (5, 3),
+--  (6, 1),
+--  (6, 2),
+--  (7, 2),
+--  (8, 1),
+--  (8, 2),
+--  (8, 3),
+--  (9, 2),
+--  (10, 1),
+--  (10, 2),
+--  (10, 3),
+--  (11, 1),
+--  (11, 2),
+--  (11, 3);
  
